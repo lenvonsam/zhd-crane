4359c5532cd7e0145b5e05e9a2082c95ae993782
sam <sam@thinkingsam.cn>
2019-09-20 16:36:46 +0800

    upgrade operater code

--- static/js/index.js
+++ static/js/index.js
@@ -35,6 +35,7 @@ $(function() {
     var str = '';
     wnameMap[wname].map(itm => {
       // str += '<input class="cbx-employee mr-15 ml-15" readonly type="checkbox" data-name="'+ wname + itm+'"/><span data-name="'+ itm +'" class="cbx-name">'+itm+'</span>';
+      // wname
       str += '<div class="employ-cbx" data-name="'+ itm +'">'+itm+'</div>';
     });
     $('#topEmployee').html('')
