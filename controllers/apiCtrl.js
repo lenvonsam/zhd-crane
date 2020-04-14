fb92382bbad14a410713df337bb76f07090d4ea5
sam <sam@thinkingsam.cn>
2020-04-14 14:13:40 +0800

    change session expire time to 13h

--- controllers/apiCtrl.js
+++ controllers/apiCtrl.js
@@ -1,16 +1,16 @@
 const httpHelp = require('../utils/http')
-// const PROXYURL = 'http://192.168.80.99:8080/warehouse-dev/warehouse'
-const PROXYURL = 'http://192.168.80.102:8686/warehouse/warehouse'
+const PROXYURL = 'http://192.168.80.99:8080/warehouse-dev/warehouse'
+// const PROXYURL = 'http://192.168.80.102:8686/warehouse/warehouse'
 // const PROXYURL = 'http://localhost:7568/warehouse'
 
 module.exports = {
-  async logout (ctx) {
+  async logout(ctx) {
     await ctx.cookies.set('currentUser', null)
     ctx.body = {
       status: 0
     }
   },
-  async warehouseName (ctx) {
+  async warehouseName(ctx) {
     let val = ctx.cookies.get('currentUser') || null
     if (val) {
       let arr = val.split('|')
@@ -18,8 +18,8 @@ module.exports = {
         wname: arr[1]
       }
     } else {
-      await ctx.render("login", {
-        pageTitle: "型云吊秤登录",
+      await ctx.render('login', {
+        pageTitle: '型云吊秤登录',
         loginType: 1
       })
     }
@@ -46,7 +46,11 @@ module.exports = {
         return
       }
     }
-    if (body.url == '/outStorage' || body.url == '/outStorageAudit' || body.url == '/outStorageQuery') {
+    if (
+      body.url == '/outStorage' ||
+      body.url == '/outStorageAudit' ||
+      body.url == '/outStorageQuery'
+    ) {
       if (userArr && userArr.length == 3) {
         params.userId = userArr[1]
         if (body.url == '/outStorageQuery') {
@@ -73,17 +77,24 @@ module.exports = {
     //   }
     // }
     console.log('warehouse proxy:>>>\n', params)
-    let data = body.method === 'get' ? (await httpHelp.httpGet(PROXYURL + body.url, params)) : (await httpHelp.httpPost(PROXYURL + body.url, params))
+    let data =
+      body.method === 'get'
+        ? await httpHelp.httpGet(PROXYURL + body.url, params)
+        : await httpHelp.httpPost(PROXYURL + body.url, params)
     console.log('warehouse proxy resp:>>\n', data)
     if (body.url == '/login') {
       if (data.status == 0 && data.data != null) {
         // let str = JSON.stringify(data.data)
         // console.log('cookie 保存', data.data)
-        await ctx.cookies.set('currentUser', `${data.data.memberCode}|${data.data.operatorUserid}|${data.data.superWarehousemanFlag}`, {
-          domain: '192.168.80.202',
-          maxAge: 10 * 60 * 60 * 1000,
-          httpOnly: false
-        })
+        await ctx.cookies.set(
+          'currentUser',
+          `${data.data.memberCode}|${data.data.operatorUserid}|${data.data.superWarehousemanFlag}`,
+          {
+            domain: '192.168.80.99',
+            maxAge: 13 * 60 * 60 * 1000,
+            httpOnly: false
+          }
+        )
       }
     }
     ctx.body = data
